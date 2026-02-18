c8da7bc0c36ea248417a62c18ce5935da5c069a6
Vignesh <vignesh24cs@rnsit.ac.in>
2026-02-18 13:27:33 +0530

    JS3

---
+++ js3.js
@@ -0,0 +1,18 @@
+// let n=1000;
+// let sum=0;
+// for(let i=1;i<=n;i++)
+// {
+//     sum+=i;
+// }
+// console.log(sum);
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+let str="Vignesh AchARYA";
+for(let i of str){
+    console.log(i);
+}
