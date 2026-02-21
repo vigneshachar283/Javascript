0502ac6494caae26a014b93a4708db123c85c66a
Vignesh <vignesh24cs@rnsit.ac.in>
2026-02-21 17:18:12 +0530

    push

---
+++ js7.js
@@ -0,0 +1,23 @@
+// // let div= document.querySelector("div");
+// // console.log(div);
+
+// // let id =div.getAttribute("id");
+// // console.log(div.setAttribute("div","box"));
+
+// // let div=document.querySelector("div");
+// // let newbtn=document.createElement("button");
+// // newbtn.innerText="CLICK ME";
+// // div.append(newbtn);
+
+// // div.append(newbtn);
+
+// let newheading=document.createElement("h1");
+// newheading.innerHTML="<i>Hi,I am new</i>";
+
+// document.querySelector("body").prepend(newheading);
+
+
+let newbtn=document.createElement("button");
+newbtn.innerText="click me";
+newbtn.style.backgroundColor="red";
+newbtn.style.Color="red";
