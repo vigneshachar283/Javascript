5229cfc8b2350347144eb544731c0520adc0e86d
Vignesh <vignesh24cs@rnsit.ac.in>
2026-02-23 23:04:49 +0530

    events

---
+++ js8.js
@@ -0,0 +1,13 @@
+let btn1=document.querySelector("#btn1");
+// btn1.onclick =()=>{
+//     alert("btn1 was clicked");
+// }
+
+// btn1.onmouseover =(d) =>{
+//     console.log(d);
+//     console.log("You are inside btn");
+// }
+
+btn1.addEventListener("click",() =>{
+console.log("button 1 is clicked");
+});
